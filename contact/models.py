68315a1039852bbba0bc8c0894ea815a6b7cd092
Tiago Oliveira <contacto.tiagooliveira@gmail.com>
2023-07-28 20:05:21 +0100

    MEDIA_URL and MEDIA_ROOT settings

--- contact/models.py
+++ contact/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
