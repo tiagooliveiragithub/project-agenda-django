13a7b6fc301e1d015b008787bd442c7c07c9ff43
Tiago Oliveira <contacto.tiagooliveira@gmail.com>
2023-08-04 16:25:31 +0100

    Delete process and more

--- contact/urls.py
+++ contact/urls.py
@@ -1,3 +1,4 @@
+# type:ignore
 
 from django.urls import path
 from contact import views
@@ -5,7 +6,14 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>', views.contact, name='contact'),  # type:ignore
-    path('search/', views.search, name='search'),  # type:ignore
-    path('', views.index, name='index'),  # type:ignore
+
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),  
+
+
+    # CRUD
+    path('contact/<int:contact_id>/detail', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 ]
